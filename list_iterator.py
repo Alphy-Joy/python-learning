d1275cc18d03b8cf196a40c863f50e57882be99c
= <=>
2026-01-20 17:08:52 +0530

    studied enumerate,reversed and zip functions

---
+++ list_iterator.py
@@ -0,0 +1,75 @@
+# iterator 
+# for containers like  like list,tuple ,set, string etc python creates objects for each in memory
+# if the containers carry huge data it will consume huge memory
+# in most of the cases we dont need the enitre data, we only need subsets or few data
+# where as iterators Generates values only when needed. it wont store everything in memory
+# An iterator is an object that returns elements one at a time using next() and raises StopIteration when exhausted.
+# iterators are used to loop through data, saves memory and increase speed and flexibility
+# An iterator: - Produces values one at a time - Uses next() - Gets exhausted
+# eg - range(), zip(), map(), filter()
+
+
+
+# Iterable
+# An iterable is something you can loop over.
+# eg - list, tuple, string, set, dict, range
+
+letters = ['a','b','c'] # iterables
+#num = 123133 # not iterable . its an integer
+for l in letters:
+    print(l)
+
+print('\n----------------------------------------------\n')
+letters = ['a','b','c'] # iterables
+new_list = []
+for l in letters:
+    new_list.append(l.upper())
+print(new_list)    
+print('\n---------------------ENUMERATE()-------------------------\n')
+
+# enumerate()
+# enumerate() is a built-in Python function that returns both index (position) and value (element) at the same time while looping
+# Each value is a tuple (index, element)
+# usecase - find the exact position of a bad data
+
+letters = ['a','b','c'] # iterables
+for index,value in enumerate(letters):
+    print(index,value)
+print('\n----------------------------------------------\n')
+
+letters = ['a','b','c'] # iterables
+print(list(enumerate(letters)))
+print(list(enumerate(letters,start=2))) # we can give starting index position
+print('\n----------------------------------------------\n')
+
+letters = ['a','','c'] # iterables
+for index,value in enumerate(letters):
+    print(index,value) # find bad value
+print('\n------------------REVERSED()----------------------------\n')
+
+# reversed()
+# reversed() is a built-in Python function that returns an iterator which gives elements in reverse order.
+# It does NOT change the original data.
+letters = ['a','b','c'] # iterables
+print(reversed(letters))
+print(list(reversed(letters))) # Very Important: reversed() Returns an Iterator
+print('\n------------------REVERSED()----------------------------\n')
+letters = ['a','b','c'] # iterables
+for l in reversed(letters):
+    print(l)
+
+
+# zip()
+# comines two or more sequences into pairs() tuples 
+# Each tuple = one record (row)
+# zip() Returns an Iterator (Important)
+# zip() stops at the shortest iterable - important interview question
+print('\n------------------ZIP()----------------------------\n')
+letters = ['a','b','c']
+numbers = [1,2,3]
+print(zip(letters,numbers))# iterables
+print(list(zip(letters,numbers)))
+for l,n in zip(letters,numbers):
+    print(l,n)
+comb = list(zip(letters,numbers,'Hi')) # stops at the shortest iterable
+print(comb)
